9a0539969b536313a5e0224af024ff8d970fc0bd
Mike Nitchie <nitchie@careevolution.com>
2015-11-15 18:39:45 -0600

    Update assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,26 +1,25 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EdmundsApiSDK")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EdmundsApiSDK")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "EdmundsApiSDK" )]
+[assembly: AssemblyDescription( "Library for consuming the Edmunds.com API" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Mike Nitchie" )]
+[assembly: AssemblyProduct( "EdmundsApiSDK" )]
+[assembly: AssemblyCopyright( "Copyright ©  2015" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c11cfa75-4b95-44fe-a3c6-010b99a41fbc")]
+[assembly: Guid( "c11cfa75-4b95-44fe-a3c6-010b99a41fbc" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "0.0.1.0" )]
+[assembly: AssemblyFileVersion( "0.0.1.0" )]
